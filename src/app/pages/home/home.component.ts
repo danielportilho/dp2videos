b7aeca636292f01d428d869f9f904f9a30e6f546
Daniel de Souza Moreira Portilho <vital.dportilho@terceiros.mag.com.br>
2021-11-04 22:08:26 +0000

    aplica novo botão get started -> contac us

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -50,62 +50,63 @@ export class HomeComponent implements OnInit, OnChanges {
       {
         id: 1,
         title: 'PondLeap',
-        category: 'Take the Leap',
+        category: 'Corporate Video',
         label: 'url(../../../assets/images/pondleap.jpg',
         video: 'https://player.vimeo.com/video/630268012?h=e398909dd1'
       },
       {
         id: 2,
         title: 'Pringles',
-        category: 'Strategy',
+        category: 'Corporate Communication',
         label: 'url(../../../assets/images/pringles.jpg',
         video: 'https://player.vimeo.com/video/630265407?h=c295d118f4'
       },
       {
         id: 3,
         title: 'PepsiCo',
-        category: 'Event',
+        category: '2D Animation',
         label: 'url(../../../assets/images/pepsico.jpg',
         video: 'https://player.vimeo.com/video/630264621?h=2183ec4828'
       },
       {
         id: 4,
         title: 'Nivea',
-        category: '',
+        category: 'Product Introduction',
         label: 'url(../../../assets/images/nivea.jpg',
         video: 'https://player.vimeo.com/video/630263280?h=55289085ae'
-      },{
+      },
+      {
         id: 5,
         title: 'Nestlé',
-        category: 'CHEF',
+        category: 'Product Launch',
         label: 'url(../../../assets/images/nestle.jpg',
         video: 'https://player.vimeo.com/video/630262193?h=f7b476d449'
       },
       {
         id: 6,
         title: 'KPMG',
-        category: 'Safety',
+        category: 'Safety Procedures',
         label: 'url(../../../assets/images/kpmg.jpg',
         video: 'https://player.vimeo.com/video/630261477?h=c4dab2ed0c'
       },
       {
         id: 7,
         title: 'C8',
-        category: '',
+        category: 'Product Commercial',
         label: 'url(../../../assets/images/c8.jpg',
         video: 'https://player.vimeo.com/video/630258399?h=bdac403a8f'
       },
       {
         id: 8,
         title: 'Sew EuroDrive',
-        category: '',
+        category: 'Corporate Video',
         label: 'url(../../../assets/images/sew-eurodrive.jpg',
         video: 'https://player.vimeo.com/video/630255438?h=b2bc85c72b'
       },
       {
         id: 9,
         title: 'Almond',
-        category: 'Product launch',
+        category: 'Product Launch',
         label: 'url(../../../assets/images/almond.jpg',
         video: 'https://player.vimeo.com/video/631169480?h=832265bc47'
       }
@@ -114,38 +115,46 @@ export class HomeComponent implements OnInit, OnChanges {
       {
         id: 10,
         title: 'Mills',
-        category: 'Services',
+        category: 'Services Video',
         label: 'url(../../../assets/images/mills.jpg',
         video: 'https://player.vimeo.com/video/631197771?h=b9210fe5c9'
       },
       {
         id: 11,
         title: 'Cargill',
-        category: 'Probeef',
+        category: 'Product Video',
         label: 'url(../../../assets/images/cargill.jpg',
         video: 'https://player.vimeo.com/video/630260756?h=bccb61c262'
       },
       {
         id: 12,
         title: 'Hardox',
-        category: '',
+        category: '3D Animation',
         label: 'url(../../../assets/images/hardox.jpg',
         video: 'https://player.vimeo.com/video/586324469?h=a2d614e66e'
       },
       {
         id: 13,
         title: 'Mars',
-        category: 'Global service case',
+        category: 'Business Case',
         label: 'url(../../../assets/images/mars.jpg',
         video: 'https://player.vimeo.com/video/313148085?h=81efe30c38'
       },
       {
         id: 14,
         title: 'Level Up',
-        category: 'Jiu-jitsu promo',
+        category: 'Event Coverage',
         label: 'url(../../../assets/images/level-up.jpg',
         video: 'https://player.vimeo.com/video/631205408?h=0a8ad85be0'
-      }
+      },
+      {
+        id: 15,
+        title: 'Positivo',
+        category: '3D Animation',
+        label: 'url(../../../assets/images/positivo.jpg',
+        video: 'https://player.vimeo.com/video/585104074?h=dbdd845edd'
+      },
+
       // + 4 videos
     ];
 
